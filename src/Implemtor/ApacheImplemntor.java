cb5415d32ee297918eb0962d8721d843b1156be9
eyal-leshem <eyal_lesh@walla.co.il>
2012-08-07 11:08:21 +0300

    crl chagne

--- src/Implemtor/ApacheImplemntor.java
+++ src/Implemtor/ApacheImplemntor.java
@@ -101,6 +101,9 @@ public class ApacheImplemntor extends Implementor
     	serverKeyFilePath  =  json.getString("serverKeyFilePath");  
     	serverCRLFilePath  =  json.getString("serverCrlFilePath");
     	name="apache"; 
+		
+		
+		
 	}
 	
 	
